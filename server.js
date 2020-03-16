dedc370659f1d6845d80760f49d570d467aaa5c2
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-03-15 20:56:56 -0400

    testing latest deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -27,11 +27,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraperHW";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-/ Routes
+// Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
